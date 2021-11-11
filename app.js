97fbca263258a7a2953dea6c4c0f7d72c58ca24c
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-11-11 16:02:29 -0600

    Add a parameter to greet function

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ new Vue({
   el: '#app',
   data: {
     title: 'Becoming a Vue Ninja',
+    name: 'Ryu',
   },
   methods: {
-    greet() {
-      return `Hello there`;
+    greet(time) {
+      return `Hello and good ${time}, ${this.name},`;
     },
   },
 });
