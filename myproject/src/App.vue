550d7ce87cec58779acbee7cbd74608d0118f4cf
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-11-12 15:28:16 -0600

    Add Blogs component

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar';
+import Blogs from './Blogs';
 
 export default {
   name: 'App',
   components: {
     Navbar,
+    Blogs,
   },
   data() {
     return {
---
+++ myproject/src/Blogs.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="blogs">
+    <h2>Blogs</h2>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Blogs',
+  data() {
+    return {
+      blogTitle: 'Blogs',
+      posts: [],
+    };
+  },
+  methods: {},
+  created() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/postss')
+      .then((response) => {
+        this.posts = response.data;
+      })
+      .catch((err) => {
+        console.log('ERROR: ', err);
+      });
+  },
+};
+</script>
