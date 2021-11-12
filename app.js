dc0e0615bf0569db37295454f86f62a1bc55fb7c
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-11-12 10:01:13 -0600

    Output items using v-for and index

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ new Vue({
   data: {
     title: 'Becoming a Vue Ninja',
     items: ['Mushroom', 'Green Sheels', 'Red Shells', 'Banana', 'Star'],
+    ninjas: [
+      { name: 'Crystal', age: 25, belt: 'Black' },
+      { name: 'Ryu', age: 30, belt: 'Brown' },
+      { name: 'Ken', age: 35, belt: 'Orange' },
+    ],
   },
   methods: {},
 });
