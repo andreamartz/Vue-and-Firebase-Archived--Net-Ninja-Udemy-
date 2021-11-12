b359c19cc9f0f8db2699ddc79e649a71d16fc1ca
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-11-12 13:08:10 -0600

    Add new Vue app

---
+++ myproject/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+    <AllFriends />
+    <OnlineFriends />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar';
+import AllFriends from './AllFriends';
+import OnlineFriends from './OnlineFriends';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    AllFriends,
+    OnlineFriends,
+  },
+  data() {
+    return {
+      title: 'My first Vue app, wooo :)',
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
