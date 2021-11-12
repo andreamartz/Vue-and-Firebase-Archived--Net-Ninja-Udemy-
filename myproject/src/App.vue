e3f7ddd8f4950aefdf397e09af3a5fb67c44fe64
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-11-12 13:50:58 -0600

    Remove AllFriends and OnlineFriends components

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,43 +2,23 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar';
-import AllFriends from './AllFriends';
-import OnlineFriends from './OnlineFriends';
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    AllFriends,
-    OnlineFriends,
   },
   data() {
     return {
       title: 'My first Vue app, wooo :)',
-      friends: [
-        { name: 'Mario', online: true },
-        { name: 'Luigi', online: false },
-        { name: 'Toad', online: true },
-        { name: 'Bowser', online: false },
-      ],
     };
   },
-  methods: {
-    deleteFriend(payload) {
-      console.log('PAYLOAD: ', payload);
-      this.friends = this.friends.filter(
-        (friend) => friend.name !== payload.name
-      );
-      console.log(this.friends);
-    },
-  },
+  methods: {},
 };
 </script>
 
